8185401a8d2d4acd4b918167c1270c55316714b8
danJ <qiaojiahui118@gmail.com>
2025-04-19 19:31:01 +0800

    refactor(env): 更新环境变量配置并优化路径解析逻辑
    重构环境变量配置，添加 `VITE_APP_ENV` 变量以区分不同环境。优化 `pathResolver` 模块的路径解析逻辑，支持非打包环境的路径处理。同时，更新 `electron-builder.json` 配置，简化文件路径定义，并添加 `extraMetadata` 字段。

--- src/electron/pathResolver.ts
+++ src/electron/pathResolver.ts
@@ -1,14 +1,24 @@
 import path from 'node:path'
 import { app } from 'electron'
-
+import { isNotDist } from './util'
 
 export function getPresloadPath() {
-    return path.join(app.getAppPath(), '/dist-electron/preload.cjs')
+  return path.join(
+    app.getAppPath(),
+    isNotDist() ? '.' : '..',
+    '/dist-electron/preload.cjs'
+  )
 }
 export function getUIPath() {
-    return path.join(app.getAppPath(), '/dist-frontend/index.html')
+  return path.join(app.getAppPath(), '/dist-frontend/index.html')
 }
-
+export function getAssetPath() {
+  return path.join(app.getAppPath(), isNotDist() ? '.' : '..', '/src/assets')
+}
 export function getIconPath() {
-    return path.join(app.getAppPath(), '/src/assets/icons')
+  return path.join(
+    app.getAppPath(),
+    isNotDist() ? '.' : '..',
+    '/src/assets/icons'
+  )
 }
--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,16 +1,26 @@
-import { ipcMain, WebFrameMain } from "electron"
-import { getUIPath } from "./pathResolver"
-import { pathToFileURL } from "url";
+import { ipcMain, WebFrameMain } from 'electron'
+import { getUIPath } from './pathResolver'
+import { pathToFileURL } from 'url'
 
-export function isDev(): boolean {
-  return process.env.NODE_ENV === 'development'
+export function ISURL(): boolean {
+  return process.env.ISURL === 'true'
+}
+/**
+ * 用于控制是否是打包后的环境 为了解决 preload 注入的路径问题
+ * @returns
+ */
+export function isNotDist(): boolean {
+  return process.env.ISNOTDIST === 'true'
 }
 /**
  * 封装ipcMain的handle方法 用与接受 invoke 事件
- * @param key 
- * @param handler 
+ * @param key
+ * @param handler
  */
-export function ipcMainHandle<Key extends keyof TSystem.EventPayloadMapping>(key: Key, handler: () => TSystem.EventPayloadMapping[Key]) {
+export function ipcMainHandle<Key extends keyof TSystem.EventPayloadMapping>(
+  key: Key,
+  handler: () => TSystem.EventPayloadMapping[Key]
+) {
   ipcMain.handle(key, (event) => {
     // 限制事件对后台的访问
     validateEventFrame(event.senderFrame)
@@ -19,10 +29,13 @@ export function ipcMainHandle<Key extends keyof TSystem.EventPayloadMapping>(key
 }
 /**
  * 封装ipcMain的on方法 用与接受 send 事件
- * @param key 
- * @param handler 
+ * @param key
+ * @param handler
  */
-export function ipcMainOn<Key extends keyof TSystem.EventPayloadMapping>(key: Key, handler: (payload: TSystem.EventPayloadMapping[Key]) => void) {
+export function ipcMainOn<Key extends keyof TSystem.EventPayloadMapping>(
+  key: Key,
+  handler: (payload: TSystem.EventPayloadMapping[Key]) => void
+) {
   ipcMain.on(key, (event, payload) => {
     // 限制事件对后台的访问
     validateEventFrame(event.senderFrame)
@@ -32,23 +45,26 @@ export function ipcMainOn<Key extends keyof TSystem.EventPayloadMapping>(key: Ke
 
 /**
  * 封装ipcRenderer的send方法 用于发送事件
- * @param key 
- * @param webContents 
- * @param payload 
+ * @param key
+ * @param webContents
+ * @param payload
  */
-export function ipcWebContentsSend<Key extends keyof TSystem.EventPayloadMapping>(
+export function ipcWebContentsSend<
+  Key extends keyof TSystem.EventPayloadMapping
+>(
   key: Key,
   webContents: Electron.WebContents,
-  payload: TSystem.EventPayloadMapping[Key]) {
+  payload: TSystem.EventPayloadMapping[Key]
+) {
   webContents.send(key, payload)
 }
 /**
  * 验证事件的来源 是否是 主进程 安全验证
- * @param frame 
- * @returns 
+ * @param frame
+ * @returns
  */
 export function validateEventFrame(frame: WebFrameMain) {
-  if (isDev() && new URL(frame.url).host === 'localhost:5123') {
+  if (ISURL() && new URL(frame.url).host === 'localhost:5123') {
     return
   }
   if (frame.url !== pathToFileURL(getUIPath()).toString()) {
